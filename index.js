1073b353e537c0007b82d2a53bff34f8d1d71f0b
HARSHIT <89982849+Blockmecoder@users.noreply.github.com>
2023-04-25 15:01:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
 
 for (var i = 0; i < 7; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", dicl);
-  function dicl() {
+  function dic1(){
     var element = this.innerHTML;
-    switch (element) {
-      case "w":
-        new Audio("tom-1.mp3").play();
-        break;
-      case "a":
-        new Audio("tom-2.mp3").play();
-        break;
-      case "s":
-        new Audio("tom-3.mp3").play();
-        break;
-      case "d":
-        new Audio("tom-4.mp3").play();
-        break;
-      case "j":
-        new Audio("snare.mp3").play();
-        break;
-      case "k":
-        new Audio("kick-bass.mp3").play();
-        break;
-      case "l":
-        new Audio("crash.mp3").play();
-        break;
-
-
+    makesound(element);
     }
-  }
-
 }
-
-
+document.addEventListener("keydown",function(event){
+    makesound(event.key);
+}
+  );
+function makesound(new)
+{
+    switch (new)
+       {
+          case "w":
+            new Audio("tom-1.mp3").play();
+          break;
+          case "a":
+            new Audio("tom-2.mp3").play();
+          break;
+          case "s":
+            new Audio("tom-3.mp3").play();
+          break;
+          case "d":
+            new Audio("tom-4.mp3").play();
+          break;
+          case "j":
+            new Audio("snare.mp3").play();
+          break;
+          case "k":
+            new Audio("kick-bass.mp3").play();
+          break;
+          case "l":
+            new Audio("crash.mp3").play();
+          break;
+          default : console.log(new);
+       }
+}
  
